b2e2ffdc8d95ee6589b7907b042375356da385b7
Vikram <vnagrani@pindropsecurity.com>
2016-07-29 16:20:15 -0400

    Uploading files

---
+++ README.md
@@ -0,0 +1,4 @@
+We had a customer that needed a daily report of call information that they could retrieve as needed.  I set up a secure sftp server on one of our appliances and this script pulls the call informatoin for them and transfers it to the sFTP server.
+
+From there I created a cron that would do this at 12:04 every morning for the previous day.  The crontab looked like this.
+00 04 * * * fdsadmin /home/fdsadmin/flatfile/flatfile.sh 2>&1 >> /tmp/flatfile.log
---
+++ flatfile.sh
@@ -0,0 +1,20 @@
+#Deploy this via cron.  00 04 * * * fdsadmin /home/fdsadmin/flatfile/flatfile.sh 2>&1 >> /tmp/flatfile.log
+
+
+#!/bin/bash
+date=$(date +%Y-%m-%d --date="1 days ago")
+
+echo "Generating Flatfile for $date"
+
+mysql -uroot -pXXXXXXXX fds -e "select c.tid, c.start_utc,  p.cid as customer, c.pid_name as customer_name, ph.cid as agent, c.other_pid_name as agent_name, c.ref_id, sum(af.duration_sec) / 2 as duration from calls c join audio_files af using (tid) join phones p using (pid) join phones ph on (ph.pid=c.other_pid) where date(c.start_utc) = '$date' group by tid" >> /home/fdsadmin/flatfile/call_records_$date.txt
+
+if [ -e "/home/fdsadmin/flatfile/call_records_$date.txt" ]; then echo "Flatfile for $date exists.....moving on......."
+echo "Fixing Permissions for Transfer:"
+chmod 777 /home/fdsadmin/flatfile/call_records_$date.txt
+
+echo "Transferring file to AMS"
+scp /home/fdsadmin/flatfile/call_records_$date.txt 162.100.84.85:/var/lib/chroot/home/restricted
+
+else echo "Flatfile for $date doesn't exist.....Aborting"
+fi 
+
